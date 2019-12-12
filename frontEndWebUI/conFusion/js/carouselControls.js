e7caa3edad992ca2ab2c36b33d96738685bb57a4
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-12-12 07:36:24 -0800

    add carousel controls, and move js into js/

---
+++ frontEndWebUI/conFusion/js/carouselControls.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#mycarousel').carousel({interval: 2000});
+    $('#carousel-pause').click(function () {
+        $('#mycarousel').carousel('pause');
+    });
+    $('#carousel-play').click(function () {
+        $('#mycarousel').carousel('cycle');
+    });
+});
+
